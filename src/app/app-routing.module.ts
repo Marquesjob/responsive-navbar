27419c111f88f9e00c22764d190f0c44f4b58ae2
Marquesjob <patrickmarques.job@hotmail.com>
2022-08-22 15:31:26 -0300

    -feature: first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgentsComponent } from './_pages/agents/agents.component';
+import { FunctionsComponent } from './_pages/functions/functions.component';
+import { MapsComponent } from './_pages/maps/maps.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'agents', component: AgentsComponent},
+  {path: 'maps', component: MapsComponent},
+  {path: 'functions', component: FunctionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './_components/nav/nav.component';
+import { AgentsComponent } from './_pages/agents/agents.component';
+import { MapsComponent } from './_pages/maps/maps.component';
+import { FunctionsComponent } from './_pages/functions/functions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AgentsComponent,
+    MapsComponent,
+    FunctionsComponent
   ],
   imports: [
     BrowserModule,
