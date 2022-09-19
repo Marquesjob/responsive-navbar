6b3f91f47aeecd55bb0a114d354d2fd56ec9dfd3
Patrick B Marques <patrick.belfort@fengbrasil.com.br>
2022-09-19 08:30:02 -0300

    Feat: Guard implement // canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainGuardGuard } from './_guards/main-guard.guard';
 import { AgentsComponent } from './_pages/agents/agents.component';
 import { FunctionsComponent } from './_pages/functions/functions.component';
 import { MapsComponent } from './_pages/maps/maps.component';
 
 const routes: Routes = [
-  {path: 'agents', component: AgentsComponent},
-  {path: 'maps', component: MapsComponent},
-  {path: 'functions', component: FunctionsComponent}
+  {path: 'agents', component: AgentsComponent,  canActivate: [MainGuardGuard]},
+  {path: 'maps', component: MapsComponent,  canActivate: [MainGuardGuard]},
+  {path: 'functions', component: FunctionsComponent, canActivate: [MainGuardGuard]}
 ];
 
 @NgModule({
